eed2d400f964d8bdd52bd881245590890684218f
Morozov Artem <69896562+morozgit@users.noreply.github.com>
2024-12-03 15:45:22 +0300

    Update location_repository.py

--- backend/src/repositories/location_repository.py
+++ backend/src/repositories/location_repository.py
@@ -9,17 +9,19 @@ class LocationRepository(AbstractRepository):
     async def add_one(self, location: SLocation) -> int:
         async with async_session_maker() as session:
             location_dict = location.model_dump()
-            location = LocationModels(**location_dict)
-            session.add(location)
+            location_orm = LocationModels(**location_dict)
+            session.add(location_orm)
             await session.flush()
             await session.commit()
-            return location.id
+            return location_orm.id
 
-    async def get_all(db) -> list[SLocation]:
-        query = select(LocationModels)
-        location_query = await db.execute(query)
-        locations = location_query.scalars().all()
-        return locations if locations else []
+    async def get_all(self) -> list[SLocation]:
+        async with async_session_maker() as session:
+            query = select(LocationModels)
+            result = await session.execute(query)
+            locations = result.scalars().all()
+            location_schemas = [SLocation.model_validate(loc.__dict__) for loc in locations]
+            return location_schemas
 
     async def get_one(self, location_id: int) -> SLocation:
         pass
